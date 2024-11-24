39981083a6bd96274b23424409c7d777fd59961e
Erich Spaker <erichspaker@gmail.com>
2024-11-24 09:41:32 -0500

    flake.nix supports build, resolves #4
    I'm not 100% sure all deps are needed, but it builds.
    Also kept the dev shell around.
    Due to a bug in cargo's handling of lockfiles I had to add two optional
    deps, in order to keep them in the lockfile.

--- flake.nix
+++ flake.nix
@@ -1,11 +1,10 @@
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/su/surrealist/package.nix
+# https://github.com/NixOS/nixpkgs/blob/10c475aeb9d30451786d9d4319b4861dce7febca/doc/hooks/tauri.section.md#L4
+
 {
   inputs = {
     nixpkgs.url = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix/monthly";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs:
@@ -13,49 +12,103 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
+      in rec {
+        # packages.default = pkgs.rustPlatform.buildRustPackage {
+        #   pname = "venator-app";
+        #   version = "0.1.0";
 
-        rust = fenix.packages.${system};
+        #   inherit buildInputs nativeBuildInputs;
 
-        libraries = with pkgs; [
-          webkitgtk_4_1
-          gtk3
-          cairo
-          gdk-pixbuf
-          glib
-          dbus
-          openssl
-          libsoup_3
-          librsvg
-        ];
+        #   preBuild = ''
+        #     cd venator-app;
+        #     npm run build
+        #   '';
 
-        packages = with pkgs; [
-          curl
-          wget
-          pkg-config
-          dbus
-          openssl
-          glib
-          gtk3
-          libsoup_3
-          webkitgtk_4_1
-          librsvg
-        ];
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = packages;
+        #   src = pkgs.lib.cleanSource ./.;
+
+        #   cargoLock.lockFile = ./Cargo.lock;
+        # };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage rec {
+          # . . .
+          pname = "venator";
+          version = "0.2.1";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
+          };
+
+          # Assuming our app's frontend uses `npm` as a package manager
+          npmDeps = pkgs.fetchNpmDeps {
+            name = "${pname}-npm-deps-${version}";
+            src = "${src}/venator-app";
+            hash = "sha256-e+DE0eOxjTQimr75LZKVD19qHhdxtLAKHhKAucCTLpk=";
+          };
+          npmRoot = "venator-app";
+
           nativeBuildInputs = with pkgs; [
-            nixd
-            rust-analyzer
-            rust.complete.toolchain
+            # Pull in our main hook
+            cargo-tauri.hook
+
+            # Setup npm
             nodejs
+            npmHooks.npmConfigHook
+
+            # Make sure we can find our libraries
+            pkg-config
+            wrapGAppsHook4
           ];
 
-          shellHook = ''
-            export LD_LIBRARY_PATH=${
-              pkgs.lib.makeLibraryPath libraries
-            }:$LD_LIBRARY_PATH
-            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-          '';
+          buildInputs = with pkgs;
+            [
+              webkitgtk_4_1
+              gtk3
+              cairo
+              gdk-pixbuf
+              glib
+              dbus
+              openssl
+              libsoup_3
+              librsvg
+            ];
+
+          # And make sure we build there too
+          buildAndTestSubdir = "venator-app/src-tauri";
+
+          meta = {
+            description = "Venator: a log and trace viewer for Rust tracing";
+            homepage = "https://github.com/kmdreko/venator";
+            license = lib.licenses.mit;
+            # maintainers = [ ];
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          # Rust Analyzer needs to be able to find the path to default crate
+          # sources, and it can read this environment variable to do so. The
+          # `rust-src` component is required in order for this to work.
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+
+          inputsFrom = [ packages.default ];
+
+          # Development tools
+          nativeBuildInputs = with pkgs; [
+            cargo
+
+            nixd
+            rust-analyzer
+            nodejs
+          ];
+        };
+
+        checks = {
+          packagesDefault = self.packages.${system}.default;
+          devShellsDefault = self.devShells.${system}.default;
         };
       });
 }
+
